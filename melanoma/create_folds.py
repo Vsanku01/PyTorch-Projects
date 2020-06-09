8db001160160fc8e38391030df7d9860dd3d8a13
Vishnu Darshan Sanku <ethhack001@gmail.com>
2020-06-09 19:25:54 +0530

    Added Logistic Regression

--- melanoma/create_folds.py
+++ melanoma/create_folds.py
@@ -6,7 +6,17 @@ from sklearn import model_selection
 if __name__ == "__main__":
     input_path = 'data/'
     df = pd.read_csv(os.path.join(input_path,'train.csv'))
-    print(df.head())
+    df["kfold"] = -1
+    df = df.sample(frac=1).reset_index(drop=True) # Shuffle the data
+    y = df.target.values
+    kf = model_selection.StratifiedKFold(n_splits=10) # Ratio of positive and negative sample to be the same
+    for fold_,(_,_) in enumerate(kf.split(X=df,y=y)):
+        df.loc[:, 'kfold'] = fold_
+
+    df.to_csv(os.path.join(input_path,"train_folds.csv"),index = False)
+
+
+
 
     
 
